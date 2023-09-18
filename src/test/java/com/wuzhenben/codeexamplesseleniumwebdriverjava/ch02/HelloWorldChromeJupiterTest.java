737c8dbd5f3497dc123d694a79b4db93ac713e1d
Ben Wu <wubin28@gmail.com>
2023-09-18 10:32:45 +0800

    feat: Example 2-1. Hello world using Chrome and JUnit 5

---
+++ src/test/java/com/wuzhenben/codeexamplesseleniumwebdriverjava/ch02/HelloWorldChromeJupiterTest.java
@@ -0,0 +1,37 @@
+package com.wuzhenben.codeexamplesseleniumwebdriverjava.ch02;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloWorldChromeJupiterTest {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        WebDriverManager.chromedriver().driverVersion("117.0.5938.88").setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        assertThat(driver.getTitle()).contains("Selenium WebDriver");
+    }
+}
